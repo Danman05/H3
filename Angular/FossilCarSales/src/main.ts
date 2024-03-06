d1b1e3a0d32570f2edfbb10e48ff4916da54c9e5
Daniel Spurrell <danielspurrell@hotmail.com>
2024-03-06 16:12:05 +0100

    flutter

--- Angular/FossilCarSales/src/main.ts
+++ Angular/FossilCarSales/src/main.ts
@@ -5,9 +5,15 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 bootstrapApplication(AppComponent, {providers: [
-  provideHttpClient(),
-  provideAnimationsAsync()
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
 ]}
 )
   .catch((err) => console.error(err));
--- Angular/PopularECars/src/main.ts
+++ Angular/PopularECars/src/main.ts
@@ -7,10 +7,24 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideRouter(routes)
-  ]})
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]})
   .catch((err) => console.error(err));
