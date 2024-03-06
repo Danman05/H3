e97e3e6632796ce169cafdd70fe08a1b1bcb7453
Daniel Spurrell <dani423j@zbc.dk>
2024-03-06 20:29:40 +0100

    indents

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -86,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child:Consumer<CounterModel>(
-              builder: (context, countModel, child) => Column(
+          builder: (context, countModel, child) => Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -100,50 +100,52 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
 
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '${countModel.count}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:  [
-                FloatingActionButton(
-                  onPressed: () {
-                    context.read<CounterModel>().incrementCounter();
-                    if (context.read<CounterModel>().isDivisibleByThree()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Count ${countModel.count} is divisible by 3'),
-                          duration: const Duration(milliseconds: 300) ,),
-                      );
-                    }
-                  },
-                  tooltip: 'Increment',
-                  backgroundColor: Colors.deepPurple.shade300,
-                  child: const Icon(
-                    Icons.add,
-                    color: Colors.green,
-                  ),
-                ),
-                FloatingActionButton(
-                  onPressed: () => context.read<CounterModel>().decrementCounter(),
-                  tooltip: "Decrement",
-                  backgroundColor: Colors.deepPurple.shade300,
-                  child: const Icon(
-                    Icons.remove, 
-                    color: Colors.red,
-                  ),
-                ),
-              ],
-            )
-          ],
+              const Text(
+                'You have pushed the button this many times:',
               ),
+              Text(
+                '${countModel.count}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      context.read<CounterModel>().incrementCounter();
+                      
+                      if (context.read<CounterModel>().isDivisibleByThree()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Count ${countModel.count} is divisible by 3'),
+                            duration: const Duration(milliseconds: 300),
+                          ),
+                        );
+                      }
+                    },
+                    tooltip: 'Increment',
+                    backgroundColor: Colors.deepPurple.shade300,
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.green,
+                    ),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => context.read<CounterModel>().decrementCounter(),
+                    tooltip: "Decrement",
+                    backgroundColor: Colors.deepPurple.shade300,
+                    child: const Icon(
+                      Icons.remove, 
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
        // This trailing comma makes auto-formatting nicer for build methods.
